ee731e58d8f1d1dbff3b812a2397d897ed8e9e17
antonio wachave <anthonypaulino32@gmail.com>
2022-05-17 00:09:49 +0200

    ajustes do front-end

--- web/src/components/WidgetForm/stages/FeedbackContentStage.tsx
+++ web/src/components/WidgetForm/stages/FeedbackContentStage.tsx
@@ -63,7 +63,7 @@ export function FeedbackContentStage({ feedbackType, onFeedbackRestartRequested,
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="conte com detalhes o que esta acontecendo..."
+          placeholder="Conte com detalhes o que esta acontecendo..."
           onChange={event => setComment(event.target.value)}
         />
 
